d2767a0251770a4292e3ae20eac17eb02d234fe3
Brittany Hartmire <bkhartmire@gmail.com>
2019-08-13 18:12:06 +0900

    file casename

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./App";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { createStore, compose, applyMiddleware } from "redux";
