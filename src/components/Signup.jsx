27ba31840ddb80433a3ceb81dfbe1612796ea567
Brittany Hartmire <bkhartmire@gmail.com>
2019-08-16 13:54:04 +0900

    add event listeners for signup form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -3,34 +3,69 @@ import "../styles/UserForms.css";
 import { connect } from "react-redux";
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+      }
+    };
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.signupUser(this.state.userInfo);
+  }
+  handleChange(e) {
+    const field = e.target.name;
+    const userInfo = this.state.userInfo;
+    userInfo[field] = e.target.value;
+    this.setState({ userInfo });
+  }
   render() {
     return (
       <div id="signup">
-        <form className="user-form" id="signup-form">
+        <form
+          className="user-form"
+          id="signup-form"
+          onSubmit={this.handleSubmit.bind(this)}
+        >
           <div className="form-group">
             <label className="title">Sign Up</label>
-            <input name="" className="form-control" placeholder="Username" />
+            <input
+              name="username"
+              className="form-control"
+              placeholder="Username"
+              onChange={this.handleChange.bind(this)}
+            />
           </div>
           <div className="form-group">
             <input
-              name=""
+              name="email"
               className="form-control"
               placeholder="Email"
               type="email"
+              onChange={this.handleChange.bind(this)}
             />
           </div>
           <div className="form-group">
             <input
+              name="password"
               className="form-control"
               placeholder="Password"
               type="password"
+              onChange={this.handleChange.bind(this)}
             />
           </div>
           <div className="form-group">
             <input
+              name="passwordConfirmation"
               className="form-control"
               placeholder="Confirm Password"
               type="password"
+              onChange={this.handleChange.bind(this)}
             />
           </div>
           <div className="form-group">
