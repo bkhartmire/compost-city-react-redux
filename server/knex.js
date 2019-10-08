cc7866b82cc64177edaed0c1c7ce7ad0b0feb242
Brittany Hartmire <bkhartmire@gmail.com>
2019-10-08 12:57:03 -0700

    default db config vars

--- server/knex.js
+++ server/knex.js
@@ -2,18 +2,17 @@ const knex = require("knex");
 require("dotenv").config();
 
 const l = {
-  user: process.env.DB_USER,
+  user: process.env.DB_USER || "postgres",
   port: process.env.DB_PORT || 5432,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "compostcity",
+  host: process.env.DB_HOST || "localhost"
 };
 const db = knex({
   client: "pg",
   connection:
     process.env.DATABASE_URL ||
-    `postgres://${l.user}:${l.password}@${process.env.DB_HOST}:${l.port}/${
-      l.database
-    }`,
+    `postgres://${l.user}:${l.password}@${l.host}:${l.port}/${l.database}`,
   searchPath: "public",
   migration: {
     tablename: "knex_migrations",
